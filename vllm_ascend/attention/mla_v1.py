5b31d6ff7e9f6203ecc11c56afd1b7a41ca47b96
wzx0726 <zhexuanwu12@gmail.com>
2026-08-13 12:02:41 +0800

    fix(attention): add PCP enablement helper
    Signed-off-by: wzx0726 <zhexuanwu12@gmail.com>

--- vllm_ascend/attention/mla_v1.py
+++ vllm_ascend/attention/mla_v1.py
@@ -59,7 +59,6 @@ from vllm_ascend.utils import (
     get_ascend_device_type,
     maybe_trans_nz,
     weak_ref_tensors,
-    enable_pcp,
 )
 from vllm_ascend.worker.npu_input_batch import NPUInputBatch
 
